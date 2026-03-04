fd1ea977e153b945939afded72609fff28682fed
unknown <>
2026-03-04 19:28:16 +0000

    fix: remove invalid lines prop from SkeletonCard in PulsePage

--- src/pages/PulsePage.tsx
+++ src/pages/PulsePage.tsx
@@ -56,7 +56,7 @@ export function PulsePage() {
  </div>
  </div>
  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
- {[1,2,3,4].map(i => <SkeletonCard key={i} lines={3} />)}
+ {[1,2,3,4].map(i => <SkeletonCard key={i} />)}
  </div>
  </div>
  );
